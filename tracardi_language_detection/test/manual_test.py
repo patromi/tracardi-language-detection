d756a836e437845537493a027ce5f7d59d50b834
admin <patromi123@gmail.com>
2021-10-14 15:43:46 +0200

    refactoring + improving safety

--- tracardi_language_detection/test/manual_test.py
+++ tracardi_language_detection/test/manual_test.py
@@ -4,15 +4,21 @@ from tracardi.domain.event import Event
 from tracardi.domain.profile import Profile
 from tracardi.domain.session import Session
 from tracardi_plugin_sdk.service.plugin_runner import run_plugin
+from dotenv import load_dotenv
 
 from tracardi_language_detection.plugin import DetectAction
-
-init = payload={
-    'key': 'YOUR KEY',
-    'string': """Welcome aboard
+load_dotenv()
+print()
+init = payload={'source': {
+                'id': '55584df6-9ee3-4acd-a0ea-e555122f3dbc'
+                },
+                "data":{
+                "string": """Welcome aboard
         Please pay attention as we demonstrate
-        The safety features of this aircraft"""
-        }
+        The safety features of this aircraft""",
+                "key": "your key",
+                "timeout": 15}}
+
 payload = {}
 profile = Profile(id="profile-id")
 event = Event(id="event-id",
